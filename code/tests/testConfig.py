1891b4ddab3f8c059379b00058aa96e3c60107db
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-04 00:05:32 +0300

    test configuration

--- code/tests/testConfig.py
+++ code/tests/testConfig.py
@@ -1,4 +1,6 @@
-user: 'spAdmin'
-password: 'Abcd@1234'
-host: '34.87.39.39'
-database: 'sideProjectDB'
+config ={
+    'user': 'spAdmin',
+    'password': 'Abcd@1234',
+    'host': '34.87.39.39',
+    'database': 'sideProjectDB'
+}
