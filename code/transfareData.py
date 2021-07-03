9c1fbf36f52333739f19ed8effc81bb8ae8ac7c9
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-04 00:03:17 +0300

    create table script solution

--- code/transfareData.py
+++ code/transfareData.py
@@ -13,6 +13,9 @@ cursor = dbConnection.cursor()
 #check if connection established!
 print(f'Connection ... \n{dbConnection}')
 
+#creating database table
+cursor.execute("CREATE TABLE corona_analysis (iso_code VARCHAR(50), continent VARCHAR(50), location VARCHAR(50), date DATE, total_cases INT(100), new_cases INT(100), total_deaths INT(100), new_deaths INT(100), icu_patients INT(100), new_tests INT(100), total_tests INT(100), positive_rate INT(100), total_vaccinations INT(100), people_vaccinated INT(100), people_fully_vaccinated INT(100), new_vaccinations INT(100), population INT(100));")
+
 #Inserting to the table
 csv_data = csv.reader(open('../datasets/owid-covid-data.csv'))
 header = next(csv_data)
@@ -20,7 +23,7 @@ print('Inserting in Process ...!')
 for row in csv_data:
     print(row)
     cursor.execute(
-        "INSERT INTO corona_analysis (iso_code,continent,location,date,total_cases,new_cases,total_deaths,new_deaths,icu_patients,new_tests,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,new_vaccinations,population,median_age,aged_65_older,aged_70_older,female_smokers,male_smokers,human_development_index) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
+        "INSERT INTO corona_analysis (iso_code,continent,location,date,total_cases,new_cases,total_deaths,new_deaths,icu_patients,new_tests,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,new_vaccinations,population) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
 
 #Outputing the results
 dbConnection.commit()
