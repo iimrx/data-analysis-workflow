161728f6c7a969dc664bbbfbdb1a94a0a99efa35
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-04 03:17:36 +0300

    replcing NaN values to Zeros

--- code/tests/testEverything.py
+++ code/tests/testEverything.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 data = pd.read_csv('/home/liquidx/Desktop/sideProjecTs/datasets/gulf.csv')
-data.drop('Unnamed: 0', axis=1, inplace=True)
+data.fillna(0, inplace=True)
 data.to_csv('/home/liquidx/Desktop/sideProjecTs/datasets/gulf.csv')
-print(len(data.columns))
-print(data.columns)
+print(data.head())
