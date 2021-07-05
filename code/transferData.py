02f9cae6a17288c7fb89b30abea2160a3ecccbb3
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-05 21:16:58 +0300

    solved psql port bug

--- code/transferData.py
+++ code/transferData.py
@@ -1,21 +1,23 @@
 import time
 import csv
 import pandas as pd
-import mysql.connector as conn
-from config import *
+import psycopg2 as conn
+import PostgreSQL_Config as config
 
 #creating connection and configuration
-dbConnection = conn.connect(**config)
+dbConnect = conn.connect(database=config.database, user=config.user,
+                            password=config.password, host=config.host, port=config.port)
 #init the cursor
-cursor = dbConnection.cursor()
+cursor = dbConnect.cursor()
 #check if connection established!
-print(f'[🔥] Connection ... \n{dbConnection}\n')
+print(f'[🔥] Connection ... \n{dbConnect}\n\
+        [✔] Connected Successfully!')
 
 def CreateInsertDB():
     #creating database table
     start = time.time()
     print('[🔥] checking if table exists or creating one ...')
-    dbquery = "CREATE TABLE IF NOT EXISTS corona_analysis (iso_code VARCHAR(50), continent VARCHAR(50), location VARCHAR(50), date DATE, total_cases INT(100) NULL, new_cases INT(100) NULL, total_deaths INT(100) NULL, new_deaths INT(100) NULL, icu_patients INT(100) NULL, new_tests INT(100) NULL, total_tests INT(100) NULL, positive_rate DOUBLE(100) NULL, total_vaccinations INT(100) NULL, people_vaccinated INT(100) NULL, people_fully_vaccinated INT(100) NULL, new_vaccinations INT(100) NULL, population INT(100) NULL, median_age INT(100) NULL, aged_65_older INT(100) NULL, aged_70_older INT(100) NULL, female_smokers INT(100) NULL, male_smokers INT(100) NULL, human_development_index INT(100) NULL);"
+    dbquery = "CREATE TABLE corona_analysis (iso_code VARCHAR(50), continent VARCHAR(50), location VARCHAR(50), date DATE, total_cases INTEGER(100) NULL, new_cases INTEGER(100) NULL, total_deaths INTEGER(100) NULL, new_deaths INTEGER(100) NULL, icu_patients INTEGER(100) NULL, new_tests INTEGER(100) NULL, total_tests INTEGER(100) NULL, positive_rate DOUBLE(100) NULL, total_vaccinations INTEGER(100) NULL, people_vaccinated INTEGER(100) NULL, people_fully_vaccinated INTEGER(100) NULL, new_vaccinations INTEGER(100) NULL, population INTEGER(100) NULL, median_age INTEGER(100) NULL, aged_65_older INTEGER(100) NULL, aged_70_older INTEGER(100) NULL, female_smokers INTEGER(100) NULL, male_smokers INTEGER(100) NULL, human_development_index INTEGER(100) NULL);"
     cursor.execute(dbquery)
     end   = time.time()
     print(f'[✔] finished checking/creating!\ntime to create/check: {round(end-start, 2)} sec\n')
@@ -29,9 +31,9 @@ def CreateInsertDB():
             "INSERT INTO corona_analysis (iso_code,continent,location,date,total_cases,new_cases,total_deaths,new_deaths,icu_patients,new_tests,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,new_vaccinations,population,median_age,aged_65_older,aged_70_older,female_smokers,male_smokers,human_development_index) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
 
     #Outputing the results
-    dbConnection.commit()
+    dbConnect.commit()
     cursor.close()
-    dbConnection.close()
+    dbConnect.close()
     print('[✔] Process Done!')
 
 CreateInsertDB()
