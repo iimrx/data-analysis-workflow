5c875c0030c0ee6375485159550b4f763a0af5b2
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-06 01:55:48 +0300

    changing bull values to zeros

--- code/tests/testEverything.py
+++ code/tests/testEverything.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 data = pd.read_csv('/home/liquidx/Desktop/sideProjecTs/datasets/gulf.csv')
-data.drop('Unnamed: 0', axis=1, inplace=True)
+data.fillna(0, inplace=True)
 data.to_csv('/home/liquidx/Desktop/sideProjecTs/datasets/gulf.csv', index=False)
-print(data.columns)
+print(data.head())
