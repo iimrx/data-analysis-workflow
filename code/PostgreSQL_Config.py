f2b818d572e9311c22d30ec557ab1aa63f3a5eaf
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-06 12:35:28 +0300

    PostgreSQL Configurations

--- code/PostgreSQL_Config.py
+++ code/PostgreSQL_Config.py
@@ -1,5 +1,7 @@
-user = 'spadmin'
-password = 'Zaak1234@'
-host = '35.239.152.44'
-database = 'sideprojectdb'
-port = 5432
+config = {
+	'user' : 'spadmin',
+	'password' : 'Zaak1234@',
+	'host' : '34.136.43.94',
+	'database' : 'sideprojectdb',
+	'port' : 5432
+}
