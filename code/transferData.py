333ef8e36520c94f99fa5aef8b2cb918ea908f50
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-06 15:24:13 +0300

    cleaning the process

--- code/transferData.py
+++ code/transferData.py
@@ -10,26 +10,50 @@ cursor = dbConnect.cursor()
 #check if connection established!
 print(f'[🔥] Connection ... \n{dbConnect}\n\
       \n[✔] Connected Successfully!')
+#declaring user information
+DBname = input("Enter New Database Name: ")
+DBtable= input("Enter New Table Name: ")
+DBuser = input("Enter New Database Username: ")
+DBpass = input("Enter New User Password: ")
+
+def CreateDBUser():
+    #create new database user using psycopg2
+    startCreateDBUser = time.time()
+    print('[🔥] Creating New database user ...')
+    dbqueryCreateUser = "CREATE USER DBuser WITH PASSWORD 'DBpass';"
+    dbqueryGrantUser  = "GRANT ALL PRIVILEGES ON DATABASE DBname TO DBuser;"
+    cursor.execute(dbqueryCreateUser, dbqueryGrantUser)
+    endCreateDBUser   = time.time()
+    print(f'[✔] Finished creating database user!\nTime to create database user: {round(endCreateDBUser-startCreateDBUser, 2)} sec\n')
+
+def CreateDB():
+    #create new database using psycopg2
+    startCreateDB = time.time()
+    print('[🔥] Checking if database exists or creating new one ...')
+    dbqueryCreate = "CREATE DATABASE DBname;"
+    cursor.execute(dbqueryCreate)
+    endCreateDB   = time.time()
+    print(f'[✔] Finished creating database!\nTime to create database: {round(endCreateDB-startCreateDB, 2)} sec\n')    
 
 def CreateInsertDB():
     #creating database table and calc time of excution
-    startCreate1 = time.time()
+    startCreate = time.time()
     print('[🔥] Checking if table exists or creating one ...')
-    dbquery = "CREATE TABLE IF NOT EXISTS corona_analysis (iso_code text, continent text, location text, date DATE, total_cases decimal NULL, new_cases decimal NULL, total_deaths decimal NULL, new_deaths decimal NULL, icu_patients decimal NULL, new_tests decimal NULL, total_tests decimal NULL, positive_rate real NULL, total_vaccinations decimal NULL, people_vaccinated decimal NULL, people_fully_vaccinated decimal NULL, new_vaccinations decimal NULL, population decimal NULL, median_age decimal NULL, aged_65_older decimal NULL, aged_70_older decimal NULL, female_smokers decimal NULL, male_smokers decimal NULL, human_development_index decimal NULL);"
+    dbquery = "CREATE TABLE IF NOT EXISTS DBtable (iso_code text, continent text, location text, date DATE, total_cases decimal NULL, new_cases decimal NULL, total_deaths decimal NULL, new_deaths decimal NULL, icu_patients decimal NULL, new_tests decimal NULL, total_tests decimal NULL, positive_rate real NULL, total_vaccinations decimal NULL, people_vaccinated decimal NULL, people_fully_vaccinated decimal NULL, new_vaccinations decimal NULL, population decimal NULL, median_age decimal NULL, aged_65_older decimal NULL, aged_70_older decimal NULL, female_smokers decimal NULL, male_smokers decimal NULL, human_development_index decimal NULL);"
     cursor.execute(dbquery)
-    endCreate1   = time.time()
-    print(f'[✔] Finished checking/creating!\nTime to create/check: {round(endCreate1-startCreate1, 2)} sec\n')
+    endCreate   = time.time()
+    print(f'[✔] Finished checking/creating Table!\nTime to create/check Table: {round(endCreate-startCreate, 2)} sec\n')
     #Inserting to the table
     csv_data = csv.reader(open('../datasets/gulf.csv'))
     header = next(csv_data)
     print('[🔥] Inserting in Process ...!')
+    startInsert = time.time()
     for row in csv_data:
         print(row)
-        startCreate2 = time.time()
         cursor.execute(
-            "INSERT INTO corona_analysis (iso_code,continent,location,date,total_cases,new_cases,total_deaths,new_deaths,icu_patients,new_tests,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,new_vaccinations,population,median_age,aged_65_older,aged_70_older,female_smokers,male_smokers,human_development_index) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
-        endCreate2   = time.time()
-        print(f'[✔] Finished inserting!\nTime to insert all data: {round(endCreate2-startCreate2, 2)} sec\n')
+            "INSERT INTO DBtable (iso_code,continent,location,date,total_cases,new_cases,total_deaths,new_deaths,icu_patients,new_tests,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,new_vaccinations,population,median_age,aged_65_older,aged_70_older,female_smokers,male_smokers,human_development_index) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
+    endInsert  = time.time()
+    print(f'[✔] Finished inserting!\nTime to insert all data: {round(endInsert-startInsert, 2)} sec\n')
 
     #Outputing the results
     dbConnect.commit()
@@ -37,4 +61,6 @@ def CreateInsertDB():
     dbConnect.close()
     print('[✔] Process Done!')
 
+CreateDBUser()
+CreateDB()
 CreateInsertDB()
