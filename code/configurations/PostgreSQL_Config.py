1701da7c96dce7756dedd13ade238bb2523b6618
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-12 16:32:05 +0300

    new database configurations

--- code/configurations/PostgreSQL_Config.py
+++ code/configurations/PostgreSQL_Config.py
@@ -1,8 +1,8 @@
 #cloud sql instance configurations(PostgreSQL)
 config = {
-	'user' : 'postgres',
-	'password' : '$%)_zPinNO++',
-	'host' : '35.197.156.122',
-	'database' : 'postgres',
-	'port' : 5432
+	'user' : 'postgres', #username as in your postgresql setup
+	'password' : '$%)_zPinNO++', #username password created on setup
+	'host' : '34.87.39.39', #host ip or domain path
+	'database' : 'postgres', #database name
+	'port' : 5432 #port used by the postgresql(5432 or 5433)
 }
