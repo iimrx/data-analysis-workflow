cd923901add537dec9ddea644a7b5746d4624b63
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-12 18:13:20 +0300

    reducing the codebase

--- code/transferData.py
+++ code/transferData.py
@@ -4,17 +4,6 @@ import pandas as pd
 import psycopg2 as conn
 from configurations.PostgreSQL_Config import config
 
-def UserInput():
-    #declaring user information
-    print('[🔥] Getting User Details ...')
-    #making the (DBname,DBtable,DBuser,DBpass) global for other functions
-    global DBname,DBtable,DBuser,DBpass
-    DBname = input("Enter New Database Name: ")
-    DBtable= input("Enter New Table Name: ")
-    DBuser = input("Enter New Username: ")
-    DBpass = input("Enter New User Password: ")
-    print('[✔] All Details Stored Successfully!\n')
-
 def DBConnect():
     #creating connection, configuration and initializing the cursor
     global cursor, DBconnect #making the cursor global for other functions
@@ -26,41 +15,27 @@ def DBConnect():
     print(f'[🔥] Connecting to Database ... \n{DBconnect}\
           \n[✔] Connected Successfully!\n')
 
-def CreateDBUser():
-    #create new database user if exists and check for it
-    try:
-        startCreateDBUser = time.time()
-        print('\n[🔥] Creating New database user ...')
-        dbqueryCreateUser = "CREATE USER {} WITH PASSWORD '{}';".format(DBuser,DBpass)
-        dbqueryGrantUser  = "GRANT ALL PRIVILEGES ON DATABASE {} TO {};".format(DBname,DBuser)
-        cursor.execute(dbqueryCreateUser, dbqueryGrantUser)
-        endCreateDBUser   = time.time()
-        print(f'[✔] Finished creating database user: {DBuser}!\nTime to create database user: {round(endCreateDBUser-startCreateDBUser, 2)} sec\n')
-    except:
-        pass
-        print(f'[✔] User: {DBuser} Exists and Have All Privileges!')
-
 def CreateDB():
     #create new database and checks if exists and handle it
     try:
         startCreateDB = time.time()
         print('[🔥] Checking if database exists or creating new one ...')
-        dbqueryCreate = "CREATE DATABASE {};".format(DBname)
+        dbqueryCreate = "CREATE DATABASE coronadb;"
         cursor.execute(dbqueryCreate)
         endCreateDB   = time.time()
-        print(f'[✔] Finished creating database: {DBname}\nTime to create database: {round(endCreateDB-startCreateDB, 2)} sec\n')
+        print(f'[✔] Finished creating database: coronadb\nTime to create database: {round(endCreateDB-startCreateDB, 2)} sec\n')
     except:
         pass
-        print(f'[✔] Database: {DBname} Exists and Can Accessed!')
+        print(f'[✔] Database: coronadb Exists and Can Accessed!')
 
 def TableInsertDB():
     #creating database table and calc time of excution
     startCreate = time.time()
     print('[🔥] Checking if table exists or creating one ...')
-    dbquery = "CREATE TABLE IF NOT EXISTS {} (iso_code text, continent text, location text, date date, total_cases decimal NULL, new_cases decimal NULL, total_deaths decimal NULL, new_deaths decimal NULL, icu_patients decimal NULL, new_tests decimal NULL, total_tests decimal NULL, positive_rate real NULL, total_vaccinations decimal NULL, people_vaccinated decimal NULL, people_fully_vaccinated decimal NULL, new_vaccinations decimal NULL, population decimal NULL, median_age decimal NULL, aged_65_older decimal NULL, aged_70_older decimal NULL, female_smokers decimal NULL, male_smokers decimal NULL, human_development_index decimal NULL);".format(DBtable)
+    dbquery = "CREATE TABLE IF NOT EXISTS ksadata (iso_code text, continent text, location text, date date, total_cases decimal NULL, new_cases decimal NULL, total_deaths decimal NULL, new_deaths decimal NULL, icu_patients decimal NULL, new_tests decimal NULL, total_tests decimal NULL, positive_rate real NULL, total_vaccinations decimal NULL, people_vaccinated decimal NULL, people_fully_vaccinated decimal NULL, new_vaccinations decimal NULL, population decimal NULL, median_age decimal NULL, aged_65_older decimal NULL, aged_70_older decimal NULL, female_smokers decimal NULL, male_smokers decimal NULL, human_development_index decimal NULL);"
     cursor.execute(dbquery)
     endCreate   = time.time()
-    print(f'[✔] Finished creating Table: {DBtable}!\nTime to create/check Table: {round(endCreate-startCreate, 2)} sec\n')
+    print(f'[✔] Finished creating Table: ksadata!\nTime to create/check Table: {round(endCreate-startCreate, 2)} sec\n')
 
     #Inserting to the table we inserted before!
     csv_data = csv.reader(open('./datasets/created/ksa.csv'))
@@ -69,7 +44,7 @@ def TableInsertDB():
     for row in csv_data:
         print(row)
         cursor.execute(
-            "INSERT INTO {} (iso_code,continent,location,date,total_cases,new_cases,total_deaths,new_deaths,icu_patients,new_tests,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,new_vaccinations,population,median_age,aged_65_older,aged_70_older,female_smokers,male_smokers,human_development_index) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)".format(DBtable), row)
+            "INSERT INTO ksadata (iso_code,continent,location,date,total_cases,new_cases,total_deaths,new_deaths,icu_patients,new_tests,total_tests,positive_rate,total_vaccinations,people_vaccinated,people_fully_vaccinated,new_vaccinations,population,median_age,aged_65_older,aged_70_older,female_smokers,male_smokers,human_development_index) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
     endInsert  = time.time()
     print(f'[✔] Finished inserting!\nTime to insert all data: {round(endInsert-startInsert, 2)} sec\n')
 
@@ -80,4 +55,4 @@ def TableInsertDB():
     print('[✔] Process Done!')
 
 #excuting all functions results!
-UserInput(),DBConnect(),CreateDBUser(),CreateDB(),TableInsertDB()
+DBConnect(),CreateDB(),TableInsertDB()
