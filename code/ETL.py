fbae3b4613952e98ac1dea36895772704e3b3125
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-13 22:26:25 +0300

    adding new date formating tot the dataset

--- code/ETL.py
+++ code/ETL.py
@@ -9,11 +9,11 @@ url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/o
 data = pd.read_csv(url)
 
 #extracting needed countries and making some data manipulation to handle null values
-ksa = data.loc[data['location']=='Saudi Arabia'] # extracting saudi arabia only data
-ksa.fillna(0, inplace=True) #replacing data after we checked to be zeros which means(not recorded)
+data = data.loc[data['location']=='Saudi Arabia'] # extracting saudi arabia only data
+data.fillna(0, inplace=True) #replacing data after we checked to be zeros which means(not recorded)
 #~60 columns is to much and not all of them are needed,
 #so we gonna only take needed data columns to a new file and stay with it
-ksa = ksa[['iso_code','continent','location','date','total_cases','new_cases',
+data = data[['iso_code','continent','location','date','total_cases','new_cases',
             'total_deaths','new_deaths','icu_patients','new_tests','total_tests',
             'positive_rate','total_vaccinations','people_vaccinated','people_fully_vaccinated',
             'new_vaccinations','population','median_age','aged_65_older','aged_70_older',
@@ -21,14 +21,14 @@ ksa = ksa[['iso_code','continent','location','date','total_cases','new_cases',
             ]]
 
 #lets change date column to date type rather than object
-ksa['date'] = pd.to_datetime(ksa['date'])
+data['date'] = pd.to_datetime(data['date']).dt.strftime('%y-%m-%d')
 
 #now lets save our new dataset to its path,
 #checking if the folder exists to overright or create new
 try:
-    ksa.to_csv('./datasets/created/ksa.csv', index=False)
+    data.to_csv('../datasets/created/ksa.csv', index=False)
 except: #if the file not exists well create new file
-    save_loc = './datasets/created/ksa.csv' #path to the file
+    save_loc = '../datasets/created/ksa.csv' #path to the file
     if not os.path.exists(save_loc): #checking for the file
         os.mkdir(save_loc) #if not create new
-    ksa.to_csv('./datasets/created/ksa.csv', index=False) #save
+    ksa.to_csv('../datasets/created/ksa.csv', index=False) #save
