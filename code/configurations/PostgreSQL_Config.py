e35dd0ab4c82d0a888bdfbf6c59664881709b894
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-16 23:01:36 +0300

    re-structured

--- code/configurations/PostgreSQL_Config.py
+++
@@ -1,8 +0,0 @@
-#cloud sql instance configurations(PostgreSQL)
-config = {
-	'user' : 'postgres', #username as in your postgresql setup
-	'password' : '$%)_zPinNO++', #username password created on setup
-	'host' : '34.87.39.39', #host ip or domain path
-	'database' : 'postgres', #database name
-	'port' : 5432 #port used by the postgresql(5432 or 5433)
-}
