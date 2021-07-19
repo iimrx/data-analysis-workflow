b3ae8da7a21399543368cb988ccfb3e7327f84bf
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-19 21:50:23 +0300

    removing credits information for more secure code

--- code/configurations/SQL_Config.py
+++ code/configurations/SQL_Config.py
@@ -1,10 +1,10 @@
 #cloud sql instance configurations(PostgreSQL)
 config1 = {
-	'user' : 'upiflosvvqddnw', #username as in your postgresql setup
-	'password' : '516b8a08f07f33ad589e28756585c3b7f7b4dd61d65918c24eb74d757fae7f16', #username password created on setup
-	'host' : 'ec2-52-45-183-77.compute-1.amazonaws.com', #host ip or domain path
-	'database' : 'dd043elu66sgq0', #database name
-	'port' : 5432 #port used by the postgresql(5432 or 5433)
+	'user' : '#', #username as in your postgresql setup
+	'password' : '#', #username password created on setup
+	'host' : '#', #host ip or domain path
+	'database' : '#', #database name
+	'port' : '#' #port used by the postgresql(5432 or 5433)
 }
 #cloud sql instance configurations(MSSQL)
 config2 = {
