e5b6de3c253ab630c68d9b571c2e835d87f0aa27
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-07-24 16:36:00 +0300

    clean code and adding sleep/echo commands

--- converter.sh
+++ converter.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 python3 ./code/etl_data.py #extraction, transformation and loading data
+echo "Finished ETL Proccess ...!"
+sleep 10
 python3 ./code/transfer_data.py #importing the data to sqlite3 db
+echo "Finished Importing To DB!"
+sleep 10
 #after we load the data to sqlite3 db, we converting it ti .sql file path to make it eaiser,
 #for us to load it to multiple RDBs.
 sqlite3 database/db/coronadb.db .dump > database/sql/coronadb.sql
+echo "Finished Converting to SQL File.!"
