945ca9e18a190a4cfbfb97199450a4b30e764ecc
Abdulrazak A. Osman <72877276+pinocchioVirus@users.noreply.github.com>
2021-08-29 20:37:12 +0300

    naming bug issue solved

--- code/etl_data.py
+++ code/etl_data.py
@@ -13,28 +13,28 @@ except Exception as e: #if the proccess of Extracting the data didn't work, will
     print(f'Error while Extracting data!\n\n{e}')
 
 try:
-    df.drop(df.index[[0]], inplace=True) #droping first row
+    data.drop(data.index[[0]], inplace=True) #droping first row
     #renaming columns to be in regex format
-    df.columns = ['id','gender','region','city','age','edu_degree','major','gpa','gpa_from','inistitute','inserted_date','grad_date','record_from']
+    data.columns = ['id','gender','region','city','age','edu_degree','major','gpa','gpa_from','inistitute','inserted_date','grad_date','record_from']
     #droping unwanted columns
-    df.drop(['record_from','grad_date','inserted_date','gpa_from','gpa'], axis=1, inplace=True)
+    data.drop(['record_from','grad_date','inserted_date','gpa_from','gpa'], axis=1, inplace=True)
     
-    df.replace('انثى','أنثى', inplace=True) #replacing names to clean missed female count
-    df.fillna('لا يوجد', inplace=True) #filling nan with 'لا يوجد' because we need it
+    data.replace('انثى','أنثى', inplace=True) #replacing names to clean missed female count
+    data.fillna('لا يوجد', inplace=True) #filling nan with 'لا يوجد' because we need it
     
     #cleaning data to be ready for reporting and dashboards
-    un_wanted = df[(df['gender'] == 'لا يوجد') | (df['edu_degree'] == 'لا يوجد') | (df['major'] == 'لا يوجد') | (df['inistitute'] == 'لا يوجد')].index #cleaning 'لايوجد' from importing columns
-    df.drop(un_wanted , inplace=True) #droping it
+    un_wanted = data[(data['gender'] == 'لا يوجد') | (data['edu_degree'] == 'لا يوجد') | (data['major'] == 'لا يوجد') | (data['inistitute'] == 'لا يوجد')].index #cleaning 'لايوجد' from importing columns
+    data.drop(un_wanted , inplace=True) #droping it
     
     #now we extracting all males and females between age 22 to 30 and graduated
-    male_grad_dbmd = df.loc[(df['gender'] == 'ذكر') & (df['age'] > 22) & (df['age'] <=30) & (df['inistitute'].str.startswith("جامعة"))]
-    female_grad_dbmd = df.loc[(df['gender'] == 'أنثى') & (df['age'] > 22) & (df['age'] <=30) & (df['inistitute'].str.startswith("جامعة"))]
+    male_grad_dbmd = data.loc[(data['gender'] == 'ذكر') & (data['age'] > 22) & (data['age'] <=30) & (data['inistitute'].str.startswith("جامعة"))]
+    female_grad_dbmd = data.loc[(data['gender'] == 'أنثى') & (data['age'] > 22) & (data['age'] <=30) & (data['inistitute'].str.startswith("جامعة"))]
     #concating two datas
     males_females_with_dbmd = pd.concat([male_grad_dbmd,female_grad_dbmd])
 except Exception as e: #if the wrangling proccess didn't work, will print error message
     print(f'Error while wrangling the data!\n\n{e}')
 
 #checking if the folder exists to overwrite or create new
-df.to_csv('../datasets/created/seekers.csv', index=False)
+data.to_csv('../datasets/created/seekers.csv', index=False)
 males_females_with_dbmd.to_csv('./datasets/created/males_and_females_with_dbmd.csv', index=False)
 print('Dataset File Saved!')
